e34f55e55a85cc624a3924c3fde9b8582cc57708
Amanbg <amanbabugautam@gmail.com>
2022-05-13 16:36:05 +0530

    UPDATE: added seller api

--- user/src/user/user.controller.ts
+++ user/src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get('list-of-seller')
+  async getSellers() {
+    const sellers = await this.userService.getSellers();
+    return sellers;
+  }
+
+//   @Get('seller-catalog/:id')
+//   async getCatalogBySeller() {
+//     const sellers = await this.userService.getCatalogBySeller();
+//     return sellers;
+//   }
+
+//   @Get('create-order/:id')
+//   async createOrderForSeller() {
+//     const sellers = await this.userService.createOrderForSeller();
+//     return sellers;
+//   }
+}
--- user/src/user/user.service.ts
+++ user/src/user/user.service.ts
@@ -1,4 +1,62 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'database';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async getSellers(): Promise<any> {
+    const sellers = await User.findAll({
+      where: {
+        type: 'seller',
+        status: 'ACTIVE',
+      },
+      attributes: ['username', 'id'],
+    });
+
+    return sellers;
+  }
+
+//   async getCatalogBySeller(payload): Promise<any> {
+//     const data = payload;
+
+//     const user = new User(data);
+
+//     const existingUsers = await User.findAndCountAll({
+//       where: {
+//         [Op.and]: [{ username: user.username }, { type: user.type }],
+//       },
+//     });
+
+//     data.status = UserStatus.ACTIVE;
+
+//     if (existingUsers.count > 0) {
+//       throw new NotAcceptableException(
+//         'User with this username and type already exists',
+//       );
+//     }
+//     await user.save();
+
+//     return user;
+//   }
+//   async createOrderForSeller(payload): Promise<any> {
+//     const data = payload;
+
+//     const user = new User(data);
+
+//     const existingUsers = await User.findAndCountAll({
+//       where: {
+//         [Op.and]: [{ username: user.username }, { type: user.type }],
+//       },
+//     });
+
+//     data.status = UserStatus.ACTIVE;
+
+//     if (existingUsers.count > 0) {
+//       throw new NotAcceptableException(
+//         'User with this username and type already exists',
+//       );
+//     }
+//     await user.save();
+
+//     return user;
+//   }
+}
